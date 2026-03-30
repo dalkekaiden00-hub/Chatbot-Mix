90b634f74ebe6cfaad90e43ae0a572378dac0ca0
Forward <forward@example.com>
2026-03-29 17:42:51 -0700

    secrets edited

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
